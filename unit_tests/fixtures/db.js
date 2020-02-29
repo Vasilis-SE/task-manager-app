e5022017d3a9e9bd71d903a7998ac62482e415c7
Vasilis-SE <vasilis.tris@gmail.com>
2020-02-29 19:21:17 +0200

    More task test cases

--- unit_tests/fixtures/db.js
+++ unit_tests/fixtures/db.js
@@ -1,13 +1,27 @@
 const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
-const User = require('../../src/models/user');
 
+const User = require('../../src/models/user');
+const Task = require('../../src/models/task');
+
+// User Mock Data
 const existingUserId = new mongoose.Types.ObjectId();
 const existingUser = {
 	_id: existingUserId,
 	name: 'existingUser',
 	email: 'terte353@example.com',
 	password: 'existingUser154489652!@!@',
+	tokens: [{
+		token: jwt.sign({ _id: existingUserId }, process.env.JWT_SECRET)
+	}] 
+};
+
+const secondExistingUserId = new mongoose.Types.ObjectId();
+const secondExistingUser = {
+	_id: secondExistingUserId,
+	name: 'tamy',
+	email: 'tamy@ohmygod.com',
+	password: '1234HACKED!@!@',
 	tokens: [{
 		token: jwt.sign({ _id: existingUserId }, process.env.JWT_SECRET)
 	}] 
@@ -24,19 +38,53 @@ const nonExistingUser = {
 	}] 
 };
 
+// Task Mock Data
+const firstTask = {
+	_id: mongoose.Types.ObjectId(),
+	description: 'Test first task',
+	completed: false,
+	userid: existingUserId
+};
+
+const secondTask = {
+	_id: mongoose.Types.ObjectId(),
+	description: 'Test second task test test',
+	completed: true,
+	userid: secondExistingUserId
+};
+
+const thirdTask = {
+	_id: mongoose.Types.ObjectId(),
+	description: 'Test third task .....',
+	completed: true,
+	userid: existingUserId
+};
+
+
 const setupDatabase = async () => {
 	await User.deleteMany();
+	await Task.deleteMany();
 	
 	// After wipping the whole users document insert a new one
 	// to have a registry to work with. Some test may need it 
 	// such as uploading avatar image or even logging in.
 	await new User(existingUser).save(); 
+	await new User(secondExistingUser).save(); 
+	
+	await new Task(firstTask).save(); 
+	await new Task(secondTask).save(); 
+	await new Task(thirdTask).save(); 
 };
 
 module.exports = {
 	existingUserId,
 	existingUser,
+	secondExistingUserId,
+	secondExistingUser,
 	nonExistingUserId,
 	nonExistingUser,
-	setupDatabase
+	setupDatabase,
+	firstTask,
+	secondTask,
+	thirdTask
 };
--- unit_tests/task.test.js
+++ unit_tests/task.test.js
@@ -2,7 +2,16 @@ const request = require('supertest');
 
 const app = require('../src/app');
 const Task = require('../src/models/task');
-const {existingUserId ,existingUser ,setupDatabase} = require('./fixtures/db.js');
+const {
+	existingUserId,
+	existingUser, 
+	secondExistingUserI,
+	secondExistingUser,
+	setupDatabase, 
+	firstTask,
+	secondTask, 
+	thirdTask
+} = require('./fixtures/db.js');
 
 // Runs for every test case in this suite.
 beforeEach(setupDatabase);
@@ -23,10 +32,31 @@ test('Should create task for a user', async () => {
 	expect(task).not.toBeNull();
 });
 
-// test('Should not create task for a user', async () => {
-// 	await request(app)
-// 		.post('/tasks')
-// 		.set('Authorization', `Bearer ${existingUser.tokens[0].token}`)
-// 		.send({description: "A test task description", somethingWrong: false})
-// 		.expect(400)
-// });
+test('Fetch all user tasks', async () => {
+	// Fetch tasks
+	let response = await request(app)
+		.get('/tasks')
+		.set('Authorization', `Bearer ${existingUser.tokens[0].token}`)
+		.send()
+		.expect(200);
+
+	// Check if the response is not null
+	expect(response).not.toBeNull();
+
+	// Check if the current number of tasks of the first users is 2 
+	// (the default tasks that we insert the beforeEach method)
+	expect(response.body.length).toEqual(2);
+});
+
+test('Delete user task', async () => {
+	// Attempt to delete user task through another user (should fail)
+	let response = await request(app)
+		.delete('/tasks/:id')
+		.set('Authorization', `Bearer ${secondExistingUser.tokens[0].token}`)
+		.send({ id: firstTask._id })
+		.expect(500);
+
+	// Assert that the task of the user that was attempted to be deleted is still in the database.
+	let task = await Task.findById(firstTask._id);
+	expect(task).not.toBeNull();
+});
