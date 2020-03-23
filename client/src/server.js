179d887bc26596de95aefdc5edb2a235c6d53c97
Vasilis CodeX <vasilis.tris@gmail.com>
2020-03-23 22:49:42 +0200

    Build app and create servable script on client

--- client/src/server.js
+++ client/src/server.js
@@ -1,35 +1,12 @@
-// const express = require("express");
-// const cors = require("cors");
+const express = require("express");
+const port = process.env.PORT || 8080;
+const app = express();
 
-// const app = express();
+app.use(express.static(__dirname + "/dist"));
+app.get(/.*./, function (req, res) {
+    res.sendFile(__dirname + "/dist/index.html");
+});
 
-// // Parse incoming json to access on event handlers.
-// app.use(express.json());
-// app.use(cors());
-
-// const port = process.env.PORT;
-// app.listen(port, () => {
-// 	console.log("Server is up and running on port: " + port);
-// });
-
-
-var express = require('express');
-var path = require('path');
-var serveStatic = require('serve-static');
-var app = express();
-
-var port = process.env.PORT
-app.listen(port);
-
-console.log('starting project at ' + port);
-app.engine('pug', require('pug').__express);
-app.set('views', '/');
-app.set('view engine', 'pug');
-
-//Look for statics first
-app.use(serveStatic(path.join(__dirname, '/')));
-
-//Return the index for any other GET request
-app.get('/*', function (req, res) {
-    res.sendFile('/public/index.html', {root: path.join(__dirname, '../')});
+app.listen(port, () => {
+	console.log("Server is up and running on port: " + port);
 });
