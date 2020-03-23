50e2c67bbc7433000b87b7ef11ca979a9e4e2c7a
Vasilis CodeX <vasilis.tris@gmail.com>
2020-03-23 22:55:42 +0200

    Add dist to production

---
+++ client/dist/precache-manifest.35f22968faf594983a7d8f7e74482ef6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60883ced40af1136d427",
+    "url": "/css/app.da5836ed.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "ae589216befb566548b94b6dfbef6d19",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60883ced40af1136d427",
+    "url": "/js/app.d05acccc.js"
+  },
+  {
+    "revision": "e650293fd88f90fe60e3",
+    "url": "/js/chunk-2d22d746.a620434b.js"
+  },
+  {
+    "revision": "dfae1afde0367d6a35f2",
+    "url": "/js/chunk-vendors.89ecba36.js"
+  },
+  {
+    "revision": "8eff00fb23e5046e8587fb699e79e093",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
